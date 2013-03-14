e4d744d87e82052f5d21a9a64b9c91674f483f9b
Jamie F. Olson <jamie.f.olson@gmail.com>
2013-03-14 13:33:41 -0400

    Completed update of gradle plugin and test package

---
+++ gradle-plugin-jannotater/src/main/groovy/com/jfolson/gradle/jannotater/RJavaPlugin.groovy
@@ -0,0 +1,59 @@
+package com.jfolson.gradle.jannotater
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.bundling.Tar
+import org.gradle.api.tasks.bundling.Compression
+import org.gradle.api.plugins.BasePlugin
+import com.jfolson.gradle.r.RPackagePlugin
+import org.gradle.api.tasks.compile.JavaCompile
+
+class RJavaPluginExtension {
+  def String packageDir = "src"
+  def String packageName = null
+  def String buildDir = null
+}
+
+class RJavaPlugin implements Plugin<Project> {
+  void apply(Project project) {
+    project.apply(plugin: RPackagePlugin.class)
+	if (project.configurations.findByName("rjava")==null){
+		project.configurations.add("rjava")
+	}
+    //project.extensions.create("rjava",RJavaPluginExtension.class)
+	project.task("downloadJars", type:Copy){
+		project.afterEvaluate {
+			from project.configurations.rjava
+			into "${project.rpackage.srcDir}/inst/java"
+		}
+	}
+	
+	project.task("copyRPackageSource", type:Copy, dependsOn: project.downloadJars) {
+		project.afterEvaluate {
+			from project.rpackage.srcDir
+			into project.rpackage.buildDir
+		}
+	}
+	
+	project.task("generateRJava", type: JavaCompile, 
+		dependsOn :project.copyRPackageSource) {
+		project.afterEvaluate {
+			source = project.sourceSets.main.java
+			classpath = project.configurations.compile
+			destinationDir = project.rpackage.buildDir
+		}
+		options.compilerArgs = ["-processor",
+		"com.jfolson.jannotater.AnnotateRProcessor","-proc:only"]
+		doFirst {
+			//classpath = project.configurations.compile
+			//classpath += project.configurations.annotations
+		}
+	}
+	
+	project.roxygenize.rpackage.srcDir = project.rpackage.buildDir
+	project.roxygenize.dependsOn project.copyRPackageSource
+	project.roxygenize.dependsOn project.generateRJava
+  }
+}
